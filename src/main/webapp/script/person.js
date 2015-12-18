50a1478fff8f001e9e618995eb79db4fbff0e8f2
gabmed <gabmed@gmail.com>
2015-12-18 15:08:17 -0200

    Update person.js
    // Watch the whole sortInfo variable not only fields. That way it catches sort directions refreshing too

--- src/main/webapp/script/person.js
+++ src/main/webapp/script/person.js
@@ -48,7 +48,7 @@ app.controller('personsListController', function ($scope, $rootScope, personServ
 
     // Watch the sortInfo variable. If changes are detected than we need to refresh the grid.
     // This also works for the first page access, since we assign the initial sorting in the initialize section.
-    $scope.$watch('sortInfo.fields[0]', function () {
+    $scope.$watch('sortInfo', function () {
         $scope.refreshGrid();
     }, true);
 
